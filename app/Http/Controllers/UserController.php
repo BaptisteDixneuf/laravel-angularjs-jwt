7f0119c38b15e3d8b7ebd67de930fc5f1989ed30
Dixneuf Baptiste <baptistedixneuf@gmail.com>
2015-06-29 00:25:24 +0200

    Adaptation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Response;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class UserController extends Controller {
+
+    /*
+     * Permet de créer un compte
+     */
+	public function signup(){
+        $credentials = Input::only('email', 'password','name');
+
+        try {
+            $user = User::create($credentials);
+        } catch (Exception $e) {
+            return Response::json(['error' => $credentials], HttpResponse::HTTP_CONFLICT);
+        }
+
+        $token = JWTAuth::fromUser($user);
+
+        return Response::json(compact('token'));
+    }
+
+    /*
+     * Permet de se connecter
+     */
+    public function signin(){
+        $credentials = Input::only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return Response::json(false, HttpResponse::HTTP_UNAUTHORIZED);
+        }
+
+        return Response::json(compact('token'));
+    }
+
+
+
+}
